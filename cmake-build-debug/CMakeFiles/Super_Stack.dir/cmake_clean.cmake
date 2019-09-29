b4d89311b4ce83cc109e837323801c11b19b5db9
DenRen <denrenruslan@gmail.com>
2019-09-30 02:55:14 +0300

    Написал первую версию функций стека. В main просто пример их использования

---
+++ cmake-build-debug/CMakeFiles/Super_Stack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Super_Stack.dir/main.cpp.o"
+  "Super_Stack.pdb"
+  "Super_Stack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Super_Stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
